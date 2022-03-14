76a8924fa8f80acb92f84f331830031b4e1ca185
Mitt <10653144+MittWillson@users.noreply.github.com>
2022-03-14 11:00:46 +0800

    add twitter card example

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,29 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+       logo: require('./assets/logo.png')
+    }
+  },
+  metaInfo() {
+    return {
+      meta: [
+          // Twitter Card
+          {name: 'twitter:card', content: 'summary'},
+          {name: 'twitter:title', content: 'Vue Social Cards Example'},
+          {name: 'twitter:description', content: 'Vue sample site showing off Twitter and Facebook Cards.'},
+          // image must be an absolute path
+          {name: 'twitter:image', content: this.logo},
+          // Facebook OpenGraph
+          {property: 'og:title', content: 'Vue Social Cards Example'},
+          {property: 'og:site_name', content: 'Vue Example'},
+          {property: 'og:type', content: 'website'},
+          {property: 'og:image', content:  this.logo},
+          {property: 'og:description', content: 'Vue sample site showing off Twitter and Facebook Cards.'}
+      ]
+    }
   }
 }
 </script>
